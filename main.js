649120883b59da11e606c5143c61d14645655095
Benjamin Gazale <benjamingazale@gmail.com>
2022-09-08 21:45:55 -0400

    Html base ok

---
+++ main.js
@@ -0,0 +1,16 @@
+let selectedRow = null
+
+// Show alerts
+function showAlert(message, className){
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+
+div.appendChild(document.createTextNode(message));
+const container = document.querySelector('.container');
+const main = document.querySelector('.main');
+container.insertBefore(div, main);
+
+setTimeout(() => document.querySelector('.alert').remove(), 3000);
+}
+
+//Delete data
